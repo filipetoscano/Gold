4d72ffaa13f16378a9544ffca10b07eeae9784ec
Filipe Toscano <ft@itm.net>
2016-09-05 13:22:58 +0200

    + Add: Additional class libraries, with dependencies.

---
+++ src/Gold.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General information
+[assembly: AssemblyTitle( "Gold.Core" )]
+[assembly: AssemblyDescription( "Core assembly of Au/Gold DSL." )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Transition" )]
+[assembly: AssemblyProduct( "Gold" )]
+[assembly: AssemblyCopyright( "" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Additional information
+[assembly: ComVisible( false )]
+[assembly: Guid( "68c831ff-1711-4569-9438-50cebe2fdb95" )]
+
+// Version information
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
---
+++ src/Gold.Rhenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General information
+[assembly: AssemblyTitle( "Rhenium DSL" )]
+[assembly: AssemblyDescription( "DSL for Visio" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Transition" )]
+[assembly: AssemblyProduct( "Gold" )]
+[assembly: AssemblyCopyright( "" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Additional information
+[assembly: ComVisible( false )]
+[assembly: Guid( "e6b3068e-0f4c-4342-98ea-990a8d11eba0" )]
+
+// Version information
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
