172ec5e8214960992c03516726a62dc16c3fbe11
Filipe Toscano <ft@itm.net>
2016-09-05 17:16:21 +0200

    + Add: More project skeletons.

---
+++ src/Gold.Forms/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General information
+[assembly: AssemblyTitle( "Gold.Core" )]
+[assembly: AssemblyDescription( "Core assembly of Au/Gold DSL." )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Transition" )]
+[assembly: AssemblyProduct( "Gold" )]
+[assembly: AssemblyCopyright( "" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Additional information
+[assembly: ComVisible( false )]
+[assembly: Guid( "16d49ce1-c405-4b56-ba91-aca9538f13be" )]
+
+// Version information
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
